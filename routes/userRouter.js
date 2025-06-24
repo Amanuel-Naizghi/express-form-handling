cc6e9d9a552ba4c90990074a0126509c3bb6c4aa
Amanuel-Naizghi <naizghiaman@gmail.com>
2025-06-24 01:22:49 -0600

    Users can delete the added item by clicking the delete button

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,5 +5,9 @@ const usersController = require('../controllers/usersController');
 usersRouter.get('/',usersController.usersListGet);
 usersRouter.get('/create',usersController.usersCreateGet);
 usersRouter.post('/create',usersController.usersCreatePost);
+usersRouter.get("/:id/update", usersController.usersUpdateGet);
+usersRouter.post("/:id/update", usersController.usersUpdatePost);
+usersRouter.post("/:id/delete", usersController.usersDeletePost);
+
 
 module.exports = usersRouter;
